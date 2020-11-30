fdaa7a218b981c0fe6ed6c5af7a8a66007403c82
Sara Nolen <nolensara1@gmail.com>
2020-11-30 01:45:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   array.to_s if array.nil? or array.length <= 1
-  array[0..-2].join(", ") + " and " + array[-1]
+  "#{array[0..-2].join(", ")} and #{array.last}"  # ", and " + array[-1]
 
 
 end
