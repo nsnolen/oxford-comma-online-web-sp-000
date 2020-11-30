021b9f1eb8bb62ff5d9f3f69414659d8239e3792
Sara Nolen <nolensara1@gmail.com>
2020-11-30 01:12:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
- return array[0] if array.length == 1
- return array [0..-2].join(", ") + "and" + array[-1] if array.length > 1
+  return array.to_s if array.nil? or array.length <= 1
+    array[0..-2].join(", ") + " and " + array[-1]
 
 
 end
