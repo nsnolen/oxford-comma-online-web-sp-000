76d907a423b11423eef82a72d7f549d585603b46
Sara Nolen <nolensara1@gmail.com>
2020-11-30 01:03:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,6 @@
 def oxford_comma(array)
- array.join
- array.join(" and ")
- array.join(", ")
+array.each_with_index do |array, index|
+ array[0].join
+ array[0,1].join(" and ")
+ array[-1].join(", ")
 end
